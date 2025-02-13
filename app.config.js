46b23c0d65dc2f02293cb3e22e1be54473223990
John Fletcher <johnfletcher@Johns-MacBook-Pro.local>
2025-02-12 22:56:31 -0700

    updgraded build version

--- app.config.js
+++ app.config.js
@@ -14,7 +14,7 @@ module.exports = {
     },
     ios: {
       bundleIdentifier: "com.jonathanmfletcher.sporyai",
-      buildNumber: "3",
+      buildNumber: "4",
       supportsTablet: true,
       usesAppleSignIn: true,
     },
